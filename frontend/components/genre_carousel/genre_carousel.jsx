a924590074a71d2a7cd7da4e49d4f0ea8e57e8a8
your name hereJustinlf55 <Justinlfern@gmail.com>
2020-03-05 22:15:07 -0800

    started searchbar

--- frontend/components/genre_carousel/genre_carousel.jsx
+++ frontend/components/genre_carousel/genre_carousel.jsx
@@ -5,7 +5,7 @@ class GenreCarousel extends React.Component {
         super(props)
 
         this.genreMovies = [];
-        for (let i = 1; i < 9; i ++) {
+        for (let i = 1; i < 8; i ++) {
             this.genreMovies.push(this.props.fetchGenre(i));
         }
     }
@@ -15,12 +15,10 @@ class GenreCarousel extends React.Component {
     }
 
     render() {
-        console.log(this.genreMovies);
         if (!this.props.genres[0]) this.props.genres[0] = '';
 
         const { genres } = this.props;
 
-        console.log(this.genreMovies);
         return (
             <h1>Carousel</h1>
         );
